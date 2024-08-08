3706d080fbd7b295f9db391b8f61c8ae02645e1b
ADEL-DJEZIRI <adel.djeziri.42.46@gmail.com>
2024-08-08 03:49:32 +0100

    Delete lib/constants directory
    sdfgf

--- lib/constants/appImages.dart
+++
@@ -1,6 +0,0 @@
-// ignore_for_file: file_names
-
-class AppImages {
-  static String rootLotties = "assets/lotties_images"; 
-  static String select = "$rootLotties/selectimage.json";
-}
--- lib/constants/appbodytext.dart
+++
@@ -1,3 +0,0 @@
-class AppBodyText{
-static String  choosePhoto = "You can choose the image from your gallery or take an image for it";
-}
